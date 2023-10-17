ef975e320b27afb00e2bc66540dd0d7f804092d4
EmilieK1 <emiliekour1@gmail.com>
2023-10-17 19:20:34 +1030

    initialised linked list in server.c

--- server.c
+++ server.c
@@ -1,5 +1,11 @@
 // Server to accept connections from clients to recieve book data to add to the stored linked list
 
-// node->next - links to the next element in the shared list.   
-// node->book_next - links to the next item in the same book. 
-// node->next_frequent_search  (part2) links to the next item that had the search terms
+#include "server.h"
+
+int main(){
+    
+    //initialise head of linked list
+    struct Node *head = (struct Node*) malloc(sizeof(Node*));
+
+    return 0;
+}

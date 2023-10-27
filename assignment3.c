95daf555b9fc59514c1b2641b101fd68df2b05bf
Nathan <nathan.zacharjak@student.adelaide.edu.au>
2023-10-27 18:37:51 +1030

    Client now auto-disconects

--- assignment3.c
+++ assignment3.c
@@ -126,9 +126,9 @@ void readClient(int newSocket, struct Node *head, struct Node **bookHeads, int c
         }
 
         bzero(buffer, MAX_LINE);
-        printf("READ STATUS %d\n", readStatus);
+        // printf("READ STATUS %d\n", readStatus);
     }
-    printf("READ STATUS %d\n", readStatus);
+    // printf("READ STATUS %d\n", readStatus);
     if (readStatus < 0){
         // printf("SERVER: read error: %d\n", readStatus);
     } else if (readStatus == 0){
